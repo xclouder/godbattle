75fdcd44c81feb5375570f46bbb4078a272697e1
xclouder <xclouder@qq.com>
2015-10-23 18:24:48 +0800

    EventDispatcher相关研究

---
+++ Assets/Test/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {	
+	void Start ()
+	{
+		Messenger.AddListener("start game", StartGame);
+	}
+	
+	void StartGame()
+	{
+		Debug.Log("StartGame called in" + gameObject);  //This is the line that would throw an exception
+	}
+	
+	public void StartGameButtonPressed()
+	{
+		Messenger.Broadcast("start game");
+	}
+}
---
+++ Assets/Test/TestGameobj.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestGameobj : MonoBehaviour {
+
+	private static GameObject g;
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log(g.name);
+
+		if (g == null)
+		{
+			Debug.Log("give a gameObject");
+			g = gameObject;
+		}
+		else
+		{
+			Debug.Log("gameObject not null");
+		}
+	}
+	
+
+	public void T()
+	{
+		Debug.Log("g is :" + g.name);
+	}
+
+	public void Reload()
+	{
+		Application.LoadLevel(Application.loadedLevel);
+	}
+
+}
