e60c18dc2fae92ff439cf163e993408ea688ae40
xclouder <xclouder@qq.com>
2015-10-25 18:28:25 +0800

    引入NUnit进行单元测试 加入EventDispatch的简单 单元测试

---
+++ Assets/Scripts/EventDispatch/UnitTest/Editor/TestEventDispatcher.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using NUnit.Framework;
+
+[TestFixture]
+[Category("Event Dispatch Test")]
+public class TestEventDispatcher {
+	
+	[Test]
+	public void TestBaseListenAndFire()
+	{
+		int callCount = 0;
+		EventDispatcher.AddListener("TestEventA", ()=>{
+			callCount++;
+		});
+
+		EventDispatcher.Fire("TestEvent"); // not increased
+		Assert.AreEqual(0, callCount);
+		EventDispatcher.Fire("TestEventA"); // increased
+		Assert.AreEqual(1, callCount);
+
+		Assert.Throws(typeof(DispatchEventException), () => {
+			EventDispatcher.Fire<int>("TestEventA", 1);
+		} );
+
+		EventDispatcher.Fire("TestEventA"); // increased
+		Assert.AreEqual(2, callCount);
+
+	}
+	
+}
