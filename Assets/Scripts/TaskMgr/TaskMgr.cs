2528f4ddff2ce6779052aee71785bdbe77892c5b
xclouder <xclouder@qq.com>
2015-10-26 00:18:00 +0800

    add TaskMgr

---
+++ Assets/Scripts/TaskMgr/TaskMgr.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TaskMgr : MonoBehaviour {
+
+	public static void Init(GameObject gameObjectToAttach)
+	{
+		gameObjectToAttach.AddComponent<TaskMgr>();
+	}
+
+	private static TaskMgr ins;
+	public static TaskMgr Instance
+	{
+		get {
+
+			if (ins == null)
+			{
+				throw new System.InvalidOperationException("You should call Init(obj) first before get singleton");
+			}
+
+			return ins;
+		}
+	}
+
+	public static Coroutine StartCoroutine(IEnumerator routine)
+	{
+		return StartCoroutine(routine);
+	}
+}
