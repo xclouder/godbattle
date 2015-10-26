3575a1c31e8e48750db4878511e882995cee22df
xclouder <xclouder@qq.com>
2015-10-26 19:33:21 +0800

    CloudApplication init

--- Assets/Scripts/CloudApplication.cs
+++ Assets/Scripts/CloudApplication.cs
@@ -5,10 +5,24 @@ using System.Collections.Generic;
 public class CloudApplication {
 
 	private GameObject globalAppObject;
+	static private CloudApplication ins;
 
-	public void Setup()
+	public static void Initialize()
 	{
+		ins = new CloudApplication();
+
 		globalAppObject = new GameObject("Application");
 	}
 
+	public static CloudApplication Instance{
+		get {
+			if (ins == null)
+			{
+				throw new System.InvalidOperationException("you should call Initialize first.");
+			}
+			return ins;
+		}
+	}
+
+	private CloudApplication() {}
 }
---
+++ Assets/Scripts/GameLogic/InitGame.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InitGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () {
+
+
+
+	}
+
+}
