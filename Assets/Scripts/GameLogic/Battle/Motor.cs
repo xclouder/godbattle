4c406b61cace42065e137dcd305868804c2e7364
xclouder <xclouder@qq.com>
2015-12-09 15:24:32 +0800

    删除不需要的资源

--- Assets/Scripts/GameLogic/Battle/Motor.cs
+++ Assets/Scripts/GameLogic/Battle/Motor.cs
@@ -8,13 +8,42 @@ public class Motor : MonoBehaviour {
 	public float speed = 1f;
 	private CharacterController characterCtrl;
 
+	public float pickNextWaypointDist = 0.1f;
 	private Vector3 targetPos;
+
+	public float gravity = 20f;
+
+	//characterState lay here for convinient, refactor it later.
+	public enum CharacterState
+	{
+		Idle,
+		Run,
+		Attack,
+		Dead
+	}
+
+	public enum CharacterEvent
+	{
+		MoveTo,
+		Arrive,
+		DoAttack
+	}
+
+	private StateMachine<CharacterState, CharacterEvent> characterFSM;
 
 	// Use this for initialization
 	void Start () {
-
 		characterCtrl = GetComponent<CharacterController>();
 		EventDispatcher.AddListener<Vector3>(BattleConst.kEvent_MoveTo, MoveTo);
+
+		//setup fsm
+		characterFSM = new StateMachine<CharacterState, CharacterEvent>();
+		characterFSM.Initialize(CharacterState.Idle);
+		characterFSM.In(CharacterState.Idle).On(CharacterEvent.MoveTo).GoTo(CharacterState.Run);
+		characterFSM.In(CharacterState.Run).On(CharacterEvent.Arrive).GoTo(CharacterState.Idle);
+
+		characterFSM.Start();
+		characterFSM.Execute();
 	}
 
 	void Update()
@@ -25,7 +54,7 @@ public class Motor : MonoBehaviour {
 			var ray = Camera.main.ScreenPointToRay(mousePos);
 			
 			RaycastHit hit;
-			var hitted = Physics.Raycast(ray, out hit, 1000f);
+			var hitted = Physics.Raycast(ray, out hit, 1000f, LayerMask.GetMask(new string[]{"Terrain"}));
 			if (!hitted)
 			{
 				Debug.LogError("not hit terrain");
@@ -33,24 +62,46 @@ public class Motor : MonoBehaviour {
 			}
 			
 			var targetPos = hit.point;
-			Debug.Log("hit obj:" + hit.transform.gameObject.name);
 			EventDispatcher.Fire<Vector3>(BattleConst.kEvent_MoveTo, targetPos);
+
 		}
 	}
 
 	public void MoveTo(Vector3 pos)
 	{
-		var dir = pos - transform.position;
-		dir.y = pos.y;
 
-		transform.LookAt(dir);
+		//use fsm fully later.
+		characterFSM.Fire(CharacterEvent.MoveTo);
 
+		targetPos = pos;
+		pos.y = transform.position.y;
+
+		transform.LookAt(pos);
 	}
 
 
 	// Update is called once per frame
 	void FixedUpdate () {
+		if (characterFSM != null && characterFSM.CurrentStateId == CharacterState.Run)
+		{
+			if (Vector3.Distance(transform.position, targetPos) < pickNextWaypointDist)
+			{
+				//arrived
+				characterFSM.Fire(CharacterEvent.Arrive);
+			}
+			else
+			{
+				var lookAt = new Vector3(targetPos.x, transform.position.y, targetPos.z);
 
+//				Debug.Log("lookAt2: " + lookAt);
+//				transform.LookAt(lookAt);
+
+				//TODO:time.deltaTime should be replaced using a more powerful TimeSystem
+				var dir = (targetPos - transform.position);
+				characterCtrl.Move(dir.normalized * Time.deltaTime * speed - new Vector3(0f, gravity * Time.deltaTime, 0f));
+
+			}
+		}
 	}
 
 }
--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -2,7 +2,7 @@
 using System.Collections;
 
 public class Utils {
-
+	#region UI Related
 	public static void SetRectTransformSize(RectTransform trans, Vector2 newSize)
 	{
 		Vector2 oldSize = trans.rect.size;
@@ -10,4 +10,27 @@ public class Utils {
 		trans.offsetMin = trans.offsetMin - new Vector2(deltaSize.x * trans.pivot.x, deltaSize.y * trans.pivot.y);
 		trans.offsetMax = trans.offsetMax + new Vector2(deltaSize.x * (1f - trans.pivot.x), deltaSize.y * (1f - trans.pivot.y));
 	}
+	#endregion
+
+	#region 3D Related
+	public static bool GetPointInTerrain(float x, float z, out Vector3 point, string terrainLayerName = "Terrian")
+	{
+		//Mogo.Util.LoggerHelper.Debug("GetPointInTerrain");
+		RaycastHit hit;
+		//var flag = Physics.Raycast(new Vector3(x, 500, z), Vector3.down, out hit,1000, (int)LayerMask.Terrain);
+		var flag = Physics.Linecast(new Vector3(x, 1000, z), new Vector3(x, -1000, z), out hit, LayerMask.GetMask(new string[]{terrainLayerName}));
+		if (flag)
+		{
+			//Mogo.Util.LoggerHelper.Debug("hit.point: " + hit.point);
+			point = new Vector3(hit.point.x, hit.point.y + 0.2f, hit.point.z);
+			return true;
+		}
+		else
+		{
+			point = new Vector3(x, 50, z);
+			return false;
+		}
+		
+	}
+	#endregion
 }
