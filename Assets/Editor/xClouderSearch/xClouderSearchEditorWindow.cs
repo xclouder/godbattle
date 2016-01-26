93552901f9d606ec429fbc0b1391120dfd9ece15
xclouder <xclouder@qq.com>
2016-01-26 17:54:09 +0800

    Search Editor Win

--- Assets/Editor/xClouderSearch/xClouderSearchEditorWindow.cs
+++ Assets/Editor/xClouderSearch/xClouderSearchEditorWindow.cs
@@ -2,7 +2,47 @@
 using UnityEditor;
 using System.Collections;
 
-public class xClouderSearchEditorWindow : EditorWindow {
+public class XClouderSearchEditorWindow : EditorWindow {
 
+	private string searchTxt = string.Empty;
+	private bool isFirstShow = true;
+
+	private int selectedIndex = 0;
+	private Vector2 scrollPos = Vector2.zero;
+
+	[MenuItem ("Window/xClouder Search Window %.")]
+	static void Init () {
+		// Get existing open window or if none, make a new one:
+		XClouderSearchEditorWindow window = (XClouderSearchEditorWindow)EditorWindow.GetWindow (typeof (XClouderSearchEditorWindow));
+		window.Show();
+
+	}
+
+	void OnGUI () {
+
+		//search text
+		GUI.SetNextControlName("SearchTextField");
+		searchTxt = GUILayout.TextField(searchTxt, new GUILayoutOption[] { GUILayout.ExpandWidth(true) });
+
+		if (isFirstShow)
+		{
+			GUI.FocusControl("SearchTextField");
+			isFirstShow = false;
+		}
+
+		var resultList = AssetDatabase.FindAssets(searchTxt);
+
+		scrollPos = GUILayout.BeginScrollView(scrollPos, GUIStyle.none,  new GUILayoutOption[] { GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true) });
+
+		Debug.Log("result list count:" + resultList.Length);
+		if (resultList.Length > 0)
+		{
+			selectedIndex = GUILayout.SelectionGrid(selectedIndex, resultList, 1, new GUILayoutOption[] { GUILayout.ExpandWidth(true), GUILayout.ExpandHeight(true) });
+		}
+
+		GUILayout.EndScrollView();
+
+
+	}
 
 }
